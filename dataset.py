89952a8d1a3eeea28a1927699b089a99aed4502e
oleksmin <alxminchuk@gmail.com>
2022-05-17 19:43:19 +0300

    new file:   LED_dataset01.xls 	new file:   LED_dataset17.xls 	modified:   dataset.py

--- dataset.py
+++ dataset.py
@@ -5,7 +5,25 @@ import pandas as pd
 def dataset():
     random.seed(int(psutil.cpu_percent(1)*psutil.virtual_memory()[3]%psutil.virtual_memory()[4]))
 
-    
+    print('Loading data....')
+    data = pd.read_excel("LED_dataset01.xls",skiprows=6)
+    print('Loaded ',len(data),' rows and ',len(data.columns),' columns\n\n' )
+    print(data.head(5))
+
+    print(data.columns)
+
+    data['date']=pd.to_datetime(data['Местное время в Пулково (аэропорт)'],dayfirst=True)
+    x=data['date']
+    y=data['T']
+
+    plt.plot(x,y)
+    plt.show()
+
+    # shorten
+    condition = (data['date'].between('2017-11-01','2018-03-01'))
+    data_short = data[condition]
+    plt.plot(data_short['date'],data_short['T'])
+    plt.show()
 
 
 
